1b88669a933b400c7f22e0c4234ef90b741316c7
Yongliang <hi@phpecho.net>
2013-07-22 11:59:53 +0800

    Just beautify the arrow

--- SVPullToRefresh/UIScrollView+SVPullToRefresh.m
+++ SVPullToRefresh/UIScrollView+SVPullToRefresh.m
@@ -669,15 +669,15 @@ static char UIScrollViewPullToRefreshView;
 	CGContextAddRect(c, CGRectMake(5, 0, 12, 4)); // to-do: use dynamic points
 	CGContextAddRect(c, CGRectMake(5, 6, 12, 4)); // currently fixed size: 22 x 48pt
 	CGContextAddRect(c, CGRectMake(5, 12, 12, 4));
-	CGContextAddRect(c, CGRectMake(5, 18, 12, 4));
-	CGContextAddRect(c, CGRectMake(5, 24, 12, 4));
-	CGContextAddRect(c, CGRectMake(5, 30, 12, 4));
+	CGContextAddRect(c, CGRectMake(5, 18, 12, 8));
+    //	CGContextAddRect(c, CGRectMake(5, 24, 12, 4));
+    //	CGContextAddRect(c, CGRectMake(5, 30, 12, 4));
 	
 	// the arrow
-	CGContextMoveToPoint(c, 0, 34);
-	CGContextAddLineToPoint(c, 11, 48);
-	CGContextAddLineToPoint(c, 22, 34);
-	CGContextAddLineToPoint(c, 0, 34);
+	CGContextMoveToPoint(c, 0, 26);
+	CGContextAddLineToPoint(c, 11, 35);
+	CGContextAddLineToPoint(c, 22, 26);
+	CGContextAddLineToPoint(c, 0, 26);
 	CGContextClosePath(c);
 	
 	CGContextSaveGState(c);
